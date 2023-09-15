e588684fd17e27d92fbeb65034c11e14961f26a3
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-09-15 19:13:28 +0300

    api

--- src/test/java/tests/api/TestmoTestApiPost.java
+++ src/test/java/tests/api/TestmoTestApiPost.java
@@ -1,10 +1,7 @@
 package tests.api;
 
 import baseEntities.BaseTestApi;
-import configuration.ReadProperties;
-import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
-import org.apache.http.protocol.HTTP;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
@@ -23,8 +20,6 @@ public class TestmoTestApiPost extends BaseTestApi {
         logger.info(authentication);
 
         given()
-                .auth().oauth2(ReadProperties.getToken())
-                .header(HTTP.CONTENT_TYPE, ContentType.JSON)
                 .body("{\"name\": \"Run 1\", \"source\": \"frontend\"}")
                 .when()
                 .post(Endpoints.CREATE_AUTO_RUN)
