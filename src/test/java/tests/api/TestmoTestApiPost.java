57abc26c74cb84a578653bbc74d6bf21167e013d
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-09-15 19:00:20 +0300

    api

--- src/test/java/tests/api/TestmoTestApiPost.java
+++ src/test/java/tests/api/TestmoTestApiPost.java
@@ -1,46 +1,35 @@
 package tests.api;
 
 import baseEntities.BaseTestApi;
-import com.google.gson.Gson;
 import configuration.ReadProperties;
-import io.restassured.response.Response;
-import models.User;
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
-import org.openqa.selenium.json.TypeToken;
-import org.testng.Assert;
+import org.apache.http.protocol.HTTP;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 import utils.Endpoints;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-
+import static io.restassured.RestAssured.authentication;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 
 public class TestmoTestApiPost extends BaseTestApi {
 
+    static Logger logger = LogManager.getLogger(TestmoTestApiPost.class);
+
     @Test
     public void getAllUsers() {
-        User expectedUser = User.builder()
-                .name("Run 1")
-                .source("mobile")
-//                .isActive(true)
-//                .roleId(1)
-//                .role("Lead")
-                .build();
+
+        logger.info(authentication);
 
         given()
                 .auth().oauth2(ReadProperties.getToken())
+                .header(HTTP.CONTENT_TYPE, ContentType.JSON)
+                .body("{\"name\": \"Run 1\", \"source\": \"frontend\"}")
                 .when()
-                .get(Endpoints.GET_ALL_USERS)
+                .post(Endpoints.CREATE_AUTO_RUN)
                 .then()
                 .log().body()
-                .body("get(0).name", is(expectedUser.getName()))
-                .body("get(0).source", equalTo(expectedUser.getSource()))
-                .statusCode(HttpStatus.SC_OK);
+                .statusCode(HttpStatus.SC_CREATED);
     }
 }
