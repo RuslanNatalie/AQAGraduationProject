4af1f78b4636004f463dd4ddc57f0cc17943fbf8
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-09-18 20:59:54 +0300

    api

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -1,8 +1,6 @@
 package models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.gson.annotations.SerializedName;
 import lombok.*;
 
 @Getter
@@ -14,28 +12,5 @@ import lombok.*;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
     private String name;
-
-//    @EqualsAndHashCode.Exclude
-//    private int page;
-
-//    private String timezone;
-//    private String password;
-//
-//    @SerializedName(value = "date_format")
-//    @JsonProperty("is_active")
-//    private String dateFormat;
-//
-//    @SerializedName(value = "time_format")
-//    @JsonProperty("is_admin")
-//    @EqualsAndHashCode.Exclude
-//    private String timeFormat;
-//
-//    @SerializedName(value = "role_id")
-//    @JsonProperty("role_id")
-//    private int roleId;
-//
-//    private String role;
-//
-//    @EqualsAndHashCode.Exclude
-//    private String email_notifications;
+    private String timezone;
 }
