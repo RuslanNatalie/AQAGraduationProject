447d6171ecd64382316aed3520f5f430760876d5
Natalya Brago <nata_9507@mail.ru>
2023-09-30 21:52:52 +0300

    cicd

--- src/test/java/utils/Endpoints.java
+++ src/test/java/utils/Endpoints.java
@@ -4,5 +4,5 @@ public class Endpoints {
     public static final String GET_USER = "/api/v1/user";
     public static final String GET_USERS = "/api/v1/users";
     public static final String GET_ROLES = "/api/v1/roles";
-    public static final String CREATE_AUTO_RUN = "/api/v1/projects/3/automation/runs";
+    public static final String CREATE_AUTO_RUN = "/api/v1/projects/129/automation/runs";
 }
