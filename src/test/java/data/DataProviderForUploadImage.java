ae34078c9eff99ef2ff48f0b1bb0f05874fdd446
Natalya Brago <nata_9507@mail.ru>
2023-10-02 00:47:06 +0300

    fix upload file and webDriver

--- src/test/java/data/DataProviderForUploadImage.java
+++ src/test/java/data/DataProviderForUploadImage.java
@@ -1,13 +1,13 @@
 package data;
 
 import org.testng.annotations.DataProvider;
-import tests.gui.positive.UploadFileTest;
+import java.io.File;
 
 public class DataProviderForUploadImage {
 
     private static String initFilePath() {
-        String pathToFile = UploadFileTest.class.getClassLoader().getResource("photo_2023.jpg").getPath();
-        return pathToFile.substring(1, pathToFile.length());
+        File file = new File("src/test/resources/photo_2023.jpg");
+        return file.getAbsolutePath();
     }
 
     @DataProvider(name = "Data for upload image", parallel = true)
