993db378a573f875ea81e75c422a2bfefab5ff10
Natalya Brago <nata_9507@mail.ru>
2023-10-02 00:48:33 +0300

    fix upload file and webDriver

--- src/test/java/pages/StartPage.java
+++ src/test/java/pages/StartPage.java
@@ -3,7 +3,6 @@ package pages;
 import baseEntities.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import static com.codeborne.selenide.Selenide.$;
